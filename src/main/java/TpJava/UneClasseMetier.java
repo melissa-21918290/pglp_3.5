3cf23c40f44e5368190767b460f92d67f81a030f
melissa bafdel <melissabafdel@gmail.com>
2020-03-11 12:42:59 +0100

    midification

--- src/main/java/TpJava/UneClasseMetier.java
+++ src/main/java/TpJava/UneClasseMetier.java
@@ -12,3 +12,10 @@ public class UneClasseMetier {
 		
 		this.AT.affichageTemporel(": Fin de uneMethodeMetier");
 	}
+	
+	public void uneMethodeMetierSLF4J() {
+		this.AT.affichageTemporelSLF4J(": Début de uneMethodeMetier");
+		
+		this.AT.affichageTemporelSLF4J(": Fin de uneMethodeMetier");
+	}
+}
